311a38b26c893ecccaedd154421cd885e9e0dba6
Eric Willisson <ericwillisson@gmail.com>
2019-03-10 21:52:08 -0700

    Added Blogger-reading library

---
+++ www/blogger.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+from oauth2client import client
+from googleapiclient import sample_tools
+import PyRSS2Gen as RSS2
+import dateparser
+import pytz
+
+
+def rss():
+  # Authenticate and construct service.
+  service, flags = sample_tools.init(
+    ["blogger", "--noauth_local_webserver"], 'blogger', 'v3', __doc__, "/home/eric/projects/rss-groups/client_secrets.json",
+    scope='https://www.googleapis.com/auth/blogger')
+
+  blogs = service.blogs()
+
+#      blog = blogs.getByUrl(url="https://ericpublicblog.blogspot.com").execute()
+  blog = blogs.getByUrl(url="https://ericrssexperiments.blogspot.com").execute()
+
+#      code.interact(local={"client": client, "sample_tools": sample_tools,
+#                           "service": service, "users": users, "thisuser": thisuser,
+#                           "blogs": blogs, "thisusersblogs": thisusersblogs})
+#      exit()
+
+  posts = service.posts().list(blogId=blog["id"]).execute()
+
+  items = []
+  for post in posts["items"]:
+    items.append(RSS2.RSSItem(
+      title=post["title"],
+      author=post["author"]["displayName"],
+      link=post["url"],
+      description=post["content"],
+      guid=post["url"],
+      pubDate=dateparser.parse(post["published"]).replace(tzinfo=pytz.utc)))
+
+  return items
+
