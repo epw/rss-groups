d4e044615d08b25ba474ff1b4f45969242cd599d
Eric Willisson <ericwillisson@gmail.com>
2019-03-10 17:53:33 -0700

    Looking better, but author not showing up in theoldreader. cache maybe

--- www/rss-groups.cgi
+++ www/rss-groups.cgi
@@ -13,8 +13,14 @@ def rss_groups():
     print("Content-Type: text/xml\n")
 
     parsed = feedparser.parse("https://hackaday.com/blog/feed/")
+    entries = parsed.entries
     parsed = feedparser.parse("http://feeds.arstechnica.com/arstechnica/index")
-    xml = rss_io.feedparser_to_pyrss2gen(parsed)
+    entries.extend(parsed.entries)
+    entries.sort(key=lambda e: e.published_parsed)
+    xml = rss_io.feedparser_to_rss2("Combined Hackaday and Ars Technica",
+                                    "https://eric.willisson.org/rss-groups/rss-groups.cgi",
+                                    "First try at a combined feed",
+                                    entries)
     # DEEPLY BROKEN! Should actually handle UTF-8!
     print(xml.encode("ascii", "ignore").decode("ascii", "ignore"))
 
--- www/rss_io.py
+++ www/rss_io.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python3
 
 import datetime
-import PyRSS2Gen
+import PyRSS2Gen as RSS2
 
 
-def feedparser_to_pyrss2gen(parsed):
-    items = [PyRSS2Gen.RSSItem(
+def rss2_item_from_entry(entry):
+    return RSS2.RSSItem(
         title = entry.title,
 #        title = entry.title,
         link = entry.link,
         description = entry.content[0].value,
 #        description = "entry.content[0]",
+        author = entry.author + " <email@example.com>",
         guid = entry.link,
         pubDate = datetime.datetime(
             entry.published_parsed[0],
@@ -19,13 +20,14 @@ def feedparser_to_pyrss2gen(parsed):
             entry.published_parsed[3],
             entry.published_parsed[4],
             entry.published_parsed[5]))
-             for entry in parsed.entries]
-    rss = PyRSS2Gen.RSS2(
-        title = parsed.feed.title,
-#        title = "parsed.feed.title",
-        link = parsed.feed.link,
-        description = parsed.feed.description,
-#        description = "parsed.feed.description",
+
+
+def feedparser_to_rss2(title, link, description, entries):
+    items = [rss2_item_from_entry(entry) for entry in entries]
+    rss = RSS2.RSS2(
+        title = title,
+        link = link,
+        description = description,
         lastBuildDate = datetime.datetime.now(),
         items = items)
     return rss.to_xml(encoding="utf-8")
