62f26436977737cced0d1268420d3052680f5a31
Eric Willisson <ericwillisson@gmail.com>
2019-03-20 21:46:16 -0700

    Put essentials into user settings page

--- www/blog.cgi
+++ www/blog.cgi
@@ -4,9 +4,21 @@ import auth
 import group.group
 import http.cookies
 import os
+import urllib.parse
 
 import cgi, cgitb
 cgitb.enable()
+
+
+def get_group_id(cursor, user_id):
+    cursor.execute("SELECT group_id FROM group_users WHERE user_id = %s", (user_id,))
+    row = cursor.fetchone()
+    return row[0]
+
+
+def wordpress_webhook_url(user_id, group_id, auth_string):
+    return ("https://eric.willisson.org/rss-groups/wordpress-webhook.cgi?"
+            + urllib.parse.urlencode({"id": group_id, "auth": auth_string}))
 
 
 def page(user_id, auth_string):
@@ -23,11 +35,16 @@ def page(user_id, auth_string):
         print("<html><body><h1>Not authenticated</h1></body></html>")
         return
 
+    group_id = get_group_id(cursor, user_id)
+
     format_args = {"id": user.user_id,
                    "name": user.name,
                    "rss": user.rss,
                    "type": user.blog_type,
-                   "auth": user.auth()}
+                   "auth": user.auth(),
+                   "personal_rss": user.link(group_id),
+                   "wordpress_webhook": wordpress_webhook_url(user_id, group_id, auth_string),
+    }
 
     with open("blog.template.html") as f:
         print(f.read().format(**format_args))
--- www/blog.js
+++ www/blog.js
@@ -1,3 +1,4 @@
+/*
 function on_google_sign_in(auth_result) {
     console.log("on_google_sign_in");
     console.log(auth_result);
@@ -50,10 +51,61 @@ function old_sign_in(googleUser) {
 	     + "&auth=" + document.forms[0].elements.auth.value
 	     + "&url=" + document.forms[0].elements.rss.value);
 }
+*/
+
+function hide_instructions() {
+    document.querySelector(".authentication .wordpress").setAttribute("style", "display: none");
+    document.querySelector(".authentication .google").setAttribute("style", "display: none");
+}
+
+function show_instruction(class_name) {
+    console.log(document.querySelector(".authentication ." + class_name));
+    document.querySelector(".authentication ." + class_name).setAttribute("style", "display: block");
+}
+
+function type_changed() {
+    console.log("type_changed");
+    hide_instructions();
+    var type = document.querySelector("#type").value;
+    console.log(type);
+    switch(type) {
+    case "wordpress":
+	show_instruction("wordpress");
+	break;
+    case "blogger":
+	show_instruction("google");
+	break;
+    default:
+	break;
+    }
+}
+
+function copy_to_clipboard(str) {
+    const el = document.createElement('textarea');
+
+    el.value = str;
+    el.setAttribute("style", "display: none");
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+function copywpwebhook() {
+    console.log("Copy");
+    copy_to_clipboard(document.querySelector("#wpwebhook").value);
+    return false;
+}
 
 function init() {
     document.forms[0].elements.type.value = document.forms[0].elements.blogtypehidden.value;
 
+    document.querySelector("#type").onchange = type_changed;
+    type_changed();
+
+    document.querySelector("#copywpwebhookbutton").addEventListener("click", copywpwebhook);
+    
+    /*
     gapi.load("auth2", function() {
 	auth2 = gapi.auth2.init({
 	    client_id: "145363362327-tsvcsck1stldh89po1b1n68haehccc5h.apps.googleusercontent.com",
@@ -64,6 +116,7 @@ function init() {
 	auth2.grantOfflineAccess().then(on_google_sign_in);
 	return false;
     });
+    */
 }
 
 init();
